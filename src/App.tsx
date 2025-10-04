d4d422928540171608fbbc7890d03d08c3235e76
Grzegorz Szyda <grzegorz.szyda@smarterdiagnostics.com>
2025-10-05 00:15:25 +0200

    Polish theme & tooling.

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,10 @@ export const App = () => (
                 alt='Poo of Shame logo'
                 className='h-28 w-28 drop-shadow-[0_12px_30px_rgba(146,79,247,0.35)]'
             />
-            <h1 className='text-primary text-5xl font-black tracking-tight'>
+            <h1 className="text-primary font-black font-[\'Baloo 2\'] text-5xl tracking-tight drop-shadow-[0_12px_30px_rgba(146,79,247,0.35)]">
                 Poo of shame
             </h1>
-            <p className='text-primary-bg-light/80 max-w-xl text-lg text-balance'>
+            <p className='text-primary-light/80 max-w-xl text-lg text-balance'>
                 Posprzątaj swoją kupkę wstydu!
             </p>
         </div>
